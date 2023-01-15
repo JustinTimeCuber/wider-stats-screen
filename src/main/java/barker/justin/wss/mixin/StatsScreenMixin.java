83c8059742db42783f23db86f83043b50767e809
JustinTimeCuber <justintimecuber@gmail.com>
2023-01-15 12:03:10 -0500

    unfuck everything

--- src/main/java/barker/justin/wss/mixin/StatsScreenMixin.java
+++ src/main/java/barker/justin/wss/mixin/StatsScreenMixin.java
@@ -4,13 +4,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(targets = "net.minecraft.client.gui.screen.StatsScreen")
+@Mixin(net.minecraft.client.gui.screen.StatsScreen.class)
 public class StatsScreenMixin {
-	@ModifyConstant(method = "getColumnX()I", constant = @Constant(intValue = 115))
+	@ModifyConstant(method = "getColumnX(I)I", constant = @Constant(intValue = 115))
 	private int injected1(int value) {
 		return 155;
 	}
-	@ModifyConstant(method = "getColumnX()I", constant = @Constant(intValue = 40))
+	@ModifyConstant(method = "getColumnX(I)I", constant = @Constant(intValue = 40))
 	private int injected2(int value) {
 		return 80;
 	}
