8535083ff6133f56d20338306addcb478814e32b
Justin Barker <justintimecuber@gmail.com>
2023-03-30 14:34:29 -0400

    config loading improvements, bump to 0.1.2

--- src/main/java/barker/justin/wss/mixin/StatsScreenMixin.java
+++ src/main/java/barker/justin/wss/mixin/StatsScreenMixin.java
@@ -3,6 +3,7 @@ package barker.justin.wss.mixin;
 import barker.justin.wss.Main;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,6 +22,11 @@ public class StatsScreenMixin {
 	}
 	@Inject(method = "selectStatList(Lnet/minecraft/client/gui/widget/AlwaysSelectedEntryListWidget;)V", at = @At("HEAD"))
 	private void injected3(AlwaysSelectedEntryListWidget<?> list, CallbackInfo ci) {
-		Main.loadConfig();
+		String listClassName = list.getClass().getSimpleName();
+		// This is not a great way of doing this, but it works.
+		// Two separate checks for Yarn and intermediary mappings, so it works in dev environment as well as normal use.
+		if(listClassName.equals("ItemStatsListWidget") || listClassName.equals("class_4200")) {
+			Main.loadConfig();
+		}
 	}
 }
